52d5e55d26827932a284bf8d49804c644b82ddba
F1D022080_MUHAMMAD ZIDAN AZZAKI <148665939+danJidan@users.noreply.github.com>
2025-09-21 16:11:54 +0800

    Update arrayMethods_F1D022080.js

--- arrayMethods_F1D022080.js
+++ arrayMethods_F1D022080.js
@@ -13,20 +13,28 @@ const arrayHurufBesar = dataDiri.map((kata) => {
 });
 console.log("Array toUpperCase:", arrayHurufBesar);
 
+
+
 const kataPanjangLebihDari5 = dataDiri.filter((kata) => {
   return kata.length > 5;
 });
 console.log("Array filter (kata > 5):", kataPanjangLebihDari5);
+
+
 
 const totalPanjangKata = dataDiri.reduce((total, kata) => {
   return total + kata.length;
 }, 0);
 console.log("Array reduced (total panjang kata):", totalPanjangKata);
 
+
+
 const kataDitemukan = dataDiri.find((kata) => {
   return kata.startsWith("b");
 });
 console.log("Kata yang ditemukan (berawalan 'b'):", kataDitemukan);
+
+
 
 const apakahAdaKataSangatPanjang = dataDiri.some((kata) => {
   return kata.length > 10;
@@ -38,6 +46,8 @@ const apakahAdaKataPanjang4 = dataDiri.some((kata) => {
 });
 console.log("Apakah ada kata yang lebih panjang dari 4?:", apakahAdaKataPanjang4 ? "ada" : "tidak ada");
 
+
+
 const apakahSemuaKataPanjang = dataDiri.every((kata) => {
   return kata.length > 4;
 });
@@ -47,4 +57,5 @@ if (apakahSemuaKataPanjang) {
   console.log("Apakah semua kata lebih panjang dari 4?: tidak");
   const kata3 = dataDiri.filter((kata) => kata.length <= 4);
   console.log("Kata yang panjangnya kurang dari atau sama dengan 4:", kata3);
+
 }
